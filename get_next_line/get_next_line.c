9bc1878b5c12035bb206cdda7e534085fee40d3e
Iñaki <129852795+VillaBillings@users.noreply.github.com>
2024-03-13 19:46:52 +0100

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ivillanu <ivillanu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:02:46 by ivillanu          #+#    #+#             */
-/*   Updated: 2024/03/13 16:03:10 by ivillanu         ###   ########.fr       */
+/*   Updated: 2024/03/13 16:49:26 by ivillanu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ char	*get_next_line(int fd)
 {
 	char			*buf;
 	char			*buf_temp;
-	//static	char	*buf_final;
+	static	char	*buf_final;
 	int				num_jump;
 	int				chars_read;
 
@@ -42,33 +42,39 @@ char	*get_next_line(int fd)
 	buf_temp = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf_temp)
 		return (NULL);
-	if (!)
+	if (!buf_final)
+	{
+		buf_final = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!buf_final)
+			return (NULL);
+	}
+	buf = joinbuf(buf, buf_final, BUFFER_SIZE);
 	while (num_jump == 0)
 	{
 		chars_read = read(fd, buf_temp, BUFFER_SIZE);
 		buf_temp[chars_read] = '\0';
 		num_jump = ft_findnchar(buf_temp);
 		buf = joinbuf(buf, buf_temp, num_jump);
-		// ft_memlcpy(buf_final, buf_temp + num_jump + 1, BUFFER_SIZE);
-		// printf ("--->  buf: %s\n", buf_final);
+		if (num_jump != 0)
+			ft_memlcpy(buf_final, buf_temp + num_jump + 1, BUFFER_SIZE);
 	}
 	return (buf);
 }
 
-int main(void)
-{
-	int file_descriptor = open("file.txt", O_RDONLY);
-	char *line;
-	int lines = 1;
-	// Process the line
-	int count = 20;
-	while (count--)
-	{
-		line = get_next_line(file_descriptor);
-		printf("%i -> %s \n", lines++, line);
-	}
-	// Free the memory allocated for the line
-	free(line);
-	close(file_descriptor);
-	return (0);
-}
+// int main(void)
+// {
+// 	int file_descriptor = open("file.txt", O_RDONLY);
+// 	char *line;
+// 	int lines = 1;
+// 	// Process the line
+// 	int count = 10;
+// 	while (count--)
+// 	{
+// 		line = get_next_line(file_descriptor);
+// 		printf("%i -> %s \n", lines++, line);
+// 	}
+// 	// Free the memory allocated for the line
+// 	free(line);
+// 	close(file_descriptor);
+// 	return (0);
+// }
